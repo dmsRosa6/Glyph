e25e05319da65e3e770000f62d6611cda30c02b1
dmsRosa6 <dms.rosa6@gmail.com>
2025-12-31 22:09:19 +0000

    Colors

--- main/main.go
+++ main/main.go
@@ -9,18 +9,26 @@ import (
 )
 
 func main() {
+
+	c1 := *core.NewColor(255,100,100)
+	c2 := *core.NewColor(100,100,255)
 	c := canvas.NewCanvas(60,30,*core.NewColor(0,0,0),*core.NewColor(255,255,255))	
 
-	rect := canvas.NewRect(0,0,10,5,'X',true,*core.NewColor(100,100,100),*core.NewColor(20,50,100))
+	rect := canvas.NewRect(0,0,10,5,' ',true, c1,c1)
 
 	c.Init()
 	fmt.Scanln()
 	c.AddShape(rect)
 
 	var a int = 0
-	for a < 10{
+	for a < 50{
 		c.Draw()
-		rect.Translate(geom.Vector{1,1})
+		rect.Translate(geom.Vector{1,0})
+		if rect.Fg == c1 {
+			rect.Fg = c2
+		}else{
+			rect.Fg = c1
+		}
 		fmt.Scanln()
 		a++
 	}
